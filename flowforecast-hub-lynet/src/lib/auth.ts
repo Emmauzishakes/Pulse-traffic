2cd2bf9c38c72e4d083daf29313e5fc6b494a321
linetmungai <waithiralinet730@gmail.com>
2026-05-05 09:31:54 +0300

    add lib folder

---
+++ flowforecast-hub-lynet/src/lib/auth.ts
@@ -0,0 +1,19 @@
+export function signOut() {
+  console.log("signOut called");
+}
+
+export function getCurrentUser() {
+  return null;
+}
+
+export function signIn() {
+  console.log("signIn called");
+}
+
+export function signUp() {
+  console.log("signUp called");
+}
+
+export function isAuthenticated() {
+  return false;
+}
